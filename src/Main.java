0b9746377fbbb5922514a865c04c2ffe1e75e304
epgokce <142578266+epgokce@users.noreply.github.com>
2024-01-18 11:49:19 +0300

    Add Car class with basic structure and constructors

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import com.polymorphism.Car;
+
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Car car = new Car("Base car",8);
+
+        System.out.println(car.startEngine());
+
+        System.out.println(car.accelerate());
+
+        System.out.println(car.brake());
+
+
     }
 }
+
+
+import java.util.Objects;
+
